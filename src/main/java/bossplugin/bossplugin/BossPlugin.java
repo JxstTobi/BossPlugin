ee654bb071268cdc826ec309f32c710c3ef82b4f
pberg <p.bergweiler@gmail.com>
2022-10-10 21:16:11 +0200

    .

--- src/main/java/bossplugin/bossplugin/BossPlugin.java
+++ src/main/java/bossplugin/bossplugin/BossPlugin.java
@@ -1,13 +1,13 @@
 package bossplugin.bossplugin;
 
+import bossplugin.bossplugin.commands.ExampleCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BossPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getCommand("NaHoerMal").setExecutor(new ExampleCommand());
     }
 
     @Override
